210852a10a3712b9b20f4a24149e146b5480fd24
ArHiFranki <franki550@gmail.com>
2022-01-14 22:19:06 +0700

    Добавил вывод текста вопроса в UI

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Quiz : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TextMeshProUGUI questionText;
+    [SerializeField] private QuestionSO question;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        questionText.text = question.GetQuestion();
     }
 }
