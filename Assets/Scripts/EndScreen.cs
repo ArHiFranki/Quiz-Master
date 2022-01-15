f0dbf02812a5724ce33adc39708fd2d2abc85ffe
ArHiFranki <franki550@gmail.com>
2022-01-15 16:55:23 +0700

    Добавил скрипт для вывода результатов игры в UI

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndScreen : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI finalScoreText;
+    private ScoreKeeper scoreKeeper;
+
+
+    private void Start()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    public void ShowFinalScore()
+    {
+        finalScoreText.text = "Поздравляем!\nВаш результат: " + scoreKeeper.CalculateScore() + "%";
+    }
+}
